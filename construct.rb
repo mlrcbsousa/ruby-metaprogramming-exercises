ffa5f865f43eb0d54370f5614b9ba9626d45976c
Manuel Sousa <mlrcbsousa@gmail.com>
2019-05-14 23:40:53 +0200

    refactoring

--- construct.rb
+++ construct.rb
@@ -2,60 +2,54 @@ require 'rspec/autorun'
 require 'byebug'
 
 class ConStruct
-  attr_reader :temp
+  attr_reader :temp, :args
 
   def initialize(*args)
     @args = args
-    set_args
-    Object.const_set 'SIZE', args.size
-    Object.const_set 'ARGS', args
+    check_for_type_error
     @temp = Class.new
     build_class
   end
 
   private
 
-  def set_args
-    @args.each do |arg|
-      error_message = 'only String or Symbol argument types allowed'
+  def define_initialize(args, size)
+    temp.define_method('initialize') do |*values|
+      arguments = "(given #{values.size}, expected #{size})"
+      error_message = "wrong number of arguments #{arguments}"
+      raise ArgumentError, error_message if values.size != size
+
+      values.each_with_index do |value, i|
+        instance_variable_set "@#{args[i]}", value
+      end
+    end
+  end
+
+  def check_for_type_error
+    error_message = 'only String or Symbol argument types allowed'
+    args.each do |arg|
       correct_types = [Symbol, String].any? { |klass| arg.is_a?(klass) }
       raise TypeError, error_message unless correct_types
-
-      Object.const_set arg.upcase, arg
     end
   end
 
   def build_class
-    define_initialize
+    define_initialize args, args.size
     define_getters
     define_setters
   end
 
-  def define_initialize
-    temp.define_method('initialize') do |*args|
-      arguments = "(given #{args.size}, expected #{SIZE})"
-      error_message = "wrong number of arguments #{arguments}"
-      raise ArgumentError, error_message if args.size != SIZE
-
-      args.each_with_index { |arg, i| instance_variable_set "@#{ARGS[i]}", arg }
-    end
-  end
-
   def define_getters
-    ARGS.each do |arg|
+    args.each do |arg|
       temp.define_method(arg) { instance_variable_get "@#{arg}" }
     end
   end
 
   def define_setters
-    ARGS.each do |arg|
+    args.each do |arg|
       temp.define_method("#{arg}=") { |x| instance_variable_set "@#{arg}", x }
     end
   end
-
-  # Object.send(:remove_const, 'SIZE')
-  # Object.send(:remove_const, 'ARGS')
-  # @args.each { |arg| Object.send(:remove_const, arg.upcase) }
 
   class << self
     def new(*args)
@@ -88,11 +82,11 @@ describe ConStruct do
     end
 
     it 'blows up when there are diferences in amount of arguments' do
-      Foo = ConStruct.new(:bar, 'buzz')
+      Bar = ConStruct.new(:bar, 'buzz')
       error_message = 'wrong number of arguments (given 4, expected 2)'
 
       expect do
-        Foo.new('Jerry Seinfeld', 54, ['step', :forth], 1337)
+        Bar.new('Jerry Seinfeld', 54, ['step', :forth], 1337)
       end.to raise_error ArgumentError, error_message
     end
   end
